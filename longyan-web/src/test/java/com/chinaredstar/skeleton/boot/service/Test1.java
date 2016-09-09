c4ad584496afbf44b82b31f42efa43fd43c60481
xu.gaoren <xu.gaoren@chinaredstar.com>
2016-09-09 10:32:04 +0800

    删除单元测试代码

--- longyan-web/src/test/java/com/chinaredstar/skeleton/boot/service/Test1.java
+++ longyan-web/src/test/java/com/chinaredstar/skeleton/boot/service/Test1.java
@@ -1,21 +1,21 @@
 package com.chinaredstar.skeleton.boot.service;
 
 import com.chinaredstar.perseus.utils.JsonUtil;
-import com.chinaredstar.longyan.controller.UserController;
+//import com.chinaredstar.longyan.controller.UserController;
 import com.chinaredstar.longyan.vo.Result;
 import com.chinaredstar.longyan.vo.ResultCode;
-import com.chinaredstar.longyan.vo.UserVo;
+//import com.chinaredstar.longyan.vo.UserVo;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
+//import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.util.AopTestUtils;
+//import org.springframework.test.util.AopTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -37,35 +37,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class Test1 {
     private MockMvc mockMvc;
 
-    @Autowired
-    UserController userController;
+//    @Autowired
+//    UserController userController;
 
     @Autowired
     CacheManager cacheManager;
 
     @Before
     public void setup() {
-        MockitoAnnotations.initMocks(this);
-
-        this.mockMvc = MockMvcBuilders
-                .standaloneSetup(AopTestUtils.getTargetObject(userController))
-                .build();
+//        MockitoAnnotations.initMocks(this);
+//
+//        this.mockMvc = MockMvcBuilders
+//                .standaloneSetup(AopTestUtils.getTargetObject(userController))
+//                .build();
     }
 
     @Test
     public void test1() throws Exception {
-        UserVo userVo = new UserVo();
-        userVo.setId(1);
-        userVo.setName("yangguo");
-        Result result = new Result();
-        result.setCode(ResultCode.C200.getCode());
-        result.setValue(userVo);
-        String json = JsonUtil.toJson(result, false);
-
-
-        mockMvc.perform(get("/user/query/{id}", 1))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().json(json, true));
+//        UserVo userVo = new UserVo();
+//        userVo.setId(1);
+//        userVo.setName("yangguo");
+//        Result result = new Result();
+//        result.setCode(ResultCode.C200.getCode());
+//        result.setValue(userVo);
+//        String json = JsonUtil.toJson(result, false);
+//
+//
+//        mockMvc.perform(get("/user/query/{id}", 1))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().json(json, true));
     }
 }
