9c690a4bdd1e11b2073e55c1079ee6b2b77f275d
junjing.dong <junjing.dong@chinaredstar.com>
2016-09-29 12:54:08 +0800

    我的审核列表接口

--- common_biz/src/main/java/com/chinaredstar/commonBiz/bean/RedstarCommunityUpdateLog.java
+++ common_biz/src/main/java/com/chinaredstar/commonBiz/bean/RedstarCommunityUpdateLog.java
@@ -2,6 +2,9 @@ package com.chinaredstar.commonBiz.bean;
 
 import com.xiwa.base.bean.Identified;
 
+import java.util.Date;
+
+
 /**
  * Created by StevenDong on 2016/9/21
  */
@@ -9,9 +12,76 @@ public class RedstarCommunityUpdateLog extends RedstarCommunity implements Ident
 
     private int communityId;
 
-    private Integer reviewStatus=0; //审核状态
+    private Integer reviewStatus = 0; //审核状态
 
     private String editColumnName; //本次修改的字段名
+
+    private Date auditShowDate; //审核显示时间（方便前台展示非数据库字段）
+
+    public RedstarCommunityUpdateLog() {
+
+    }
+
+    public RedstarCommunityUpdateLog(RedstarCommunity community) {
+
+        this.setCommunityId(community.getId());
+        this.setAddress(community.getAddress());
+        this.setAlreadyCheckAmount(community.getAlreadyCheckAmount());
+        this.setAlreadyInputAmount(community.getAlreadyInputAmount());
+        this.setArea(community.getArea());
+        this.setAreaCode(community.getAreaCode());
+        this.setAreaMonut(community.getAreaMonut());
+        this.setBuildingAmount(community.getBuildingAmount());
+        this.setBuildingDate(community.getBuildingDate());
+        this.setCity(community.getCity());
+        this.setCityCode(community.getCityCode());
+        this.setCompleteStatus(community.getCompleteStatus());
+        this.setConstructionTypes(community.getConstructionTypes());
+        this.setCreateDate(community.getCreateDate());
+        this.setCreateEmployeeId(community.getCreateEmployeeId());
+        this.setCreateXingMing(community.getCreateXingMing());
+        this.setCurrentNo(community.getCurrentNo());
+        this.setDataBelong(community.getDataBelong());
+        this.setDeliveryTime(community.getDeliveryTime());
+        this.setDevelopers(community.getDevelopers());
+        this.setDistributionCharge(community.getDistributionCharge());
+        this.setFreeDistribution(community.getFreeDistribution());
+        this.setHotline(community.getHotline());
+        this.setInputRate(community.getInputRate());
+        this.setLatitude(community.getLatitude());
+        this.setLogo(community.getLogo());
+        this.setLongitude(community.getLongitude());
+        this.setMerchantId(community.getMerchantId());
+        this.setName(community.getName());
+        this.setOccupanyRate(community.getOccupanyRate());
+        this.setOwnerId(community.getOwnerId());
+        this.setOwnerMallId(community.getOwnerMallId());
+        this.setOwnerMallName(community.getOwnerMallName());
+        this.setOwnerXingMing(community.getOwnerXingMing());
+        this.setPickupAddress(community.getPickupAddress());
+        this.setPriceSection(community.getPriceSection());
+        this.setProgramFeatures(community.getProgramFeatures());
+        this.setPropertyName(community.getPropertyName());
+        this.setProvince(community.getProvince());
+        this.setProvinceCode(community.getProvinceCode());
+        this.setReclaimStatus(community.getReclaimStatus());
+        this.setRenovations(community.getRenovations());
+        this.setReviewStatus(community.getReviewStatus());
+        this.setRoomMount(community.getRoomMount());
+        this.setShortName(community.getShortName());
+        this.setSource(community.getSource());
+        this.setUpdateDate(community.getUpdateDate());
+        this.setUpdateEmployeeId(community.getUpdateEmployeeId());
+        this.setUpdateEmployeeXingMing(community.getUpdateEmployeeXingMing());
+    }
+
+    public Date getAuditShowDate() {
+        return auditShowDate;
+    }
+
+    public void setAuditShowDate(Date auditShowDate) {
+        this.auditShowDate = auditShowDate;
+    }
 
     public int getCommunityId() {
         return communityId;
@@ -37,53 +107,5 @@ public class RedstarCommunityUpdateLog extends RedstarCommunity implements Ident
 
     public void setEditColumnName(String editColumnName) {
         this.editColumnName = editColumnName;
-    }
-
-    public RedstarCommunityUpdateLog() {
-
-    }
-
-    public RedstarCommunityUpdateLog(RedstarCommunity community) {
-        this.setCommunityId(community.getId());
-        this.setName(community.getName());
-        this.setShortName(community.getShortName());
-        this.setAddress(community.getAddress());
-        this.setProvince(community.getProvince());
-        this.setProvinceCode(community.getProvinceCode());
-        this.setCity(community.getCity());
-        this.setCityCode(community.getCityCode());
-        this.setArea(community.getArea());
-        this.setAreaCode(community.getArea());
-        this.setAlreadyCheckAmount(community.getAlreadyCheckAmount());
-        this.setAlreadyInputAmount(community.getAlreadyInputAmount());
-        this.setHotline(community.getHotline());
-        this.setProgramFeatures(community.getProgramFeatures());
-        this.setAreaMonut(community.getAreaMonut());
-        this.setRoomMount(community.getRoomMount());
-        this.setBuildingAmount(community.getBuildingAmount());
-        this.setPriceSection(community.getPriceSection());
-        this.setBuildingDate(community.getBuildingDate());
-        this.setDevelopers(community.getDevelopers());
-        this.setCreateEmployeeId(community.getCreateEmployeeId());
-        this.setCreateXingMing(community.getCreateXingMing());
-        this.setUpdateEmployeeId(community.getUpdateEmployeeId());
-        this.setUpdateEmployeeXingMing(community.getUpdateEmployeeXingMing());
-        this.setCreateDate(community.getCreateDate());
-        this.setUpdateDate(community.getUpdateDate());
-        this.setLongitude(community.getLongitude());
-        this.setLatitude(community.getLatitude());
-        this.setOwnerId(community.getOwnerId());
-        this.setOwnerXingMing(community.getOwnerXingMing());
-        this.setOwnerMallId(community.getOwnerMallId());
-        this.setOwnerMallName(community.getOwnerMallName());
-        this.setSource(community.getSource());
-        this.setConstructionTypes(community.getConstructionTypes());
-        this.setRenovations(community.getRenovations());
-        this.setDeliveryTime(community.getDeliveryTime());
-        this.setPropertyName(community.getPropertyName());
-        this.setDataBelong(community.getDataBelong());
-        this.setReclaimStatus(community.getReclaimStatus());
-        this.setCompleteStatus(community.getCompleteStatus());
-
     }
 }
--- longyan-web/src/main/java/com/chinaredstar/longyan/web/controller/AuditController.java
+++ longyan-web/src/main/java/com/chinaredstar/longyan/web/controller/AuditController.java
@@ -3,6 +3,8 @@ package com.chinaredstar.longyan.web.controller;
 import com.chinaredstar.commonBiz.bean.RedstarCommunityUpdateLog;
 import com.chinaredstar.commonBiz.bean.constant.CommonBizConstant;
 import com.chinaredstar.commonBiz.manager.DispatchDriver;
+import com.chinaredstar.commonBiz.util.DoubleUtil;
+import com.chinaredstar.longyan.util.RateUtil;
 import com.chinaredstar.nvwaBiz.bean.NvwaEmployee;
 import com.xiwa.base.bean.PaginationDescribe;
 import com.xiwa.base.bean.Response;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 
 /**
@@ -45,6 +49,7 @@ public class AuditController extends BaseController implements CommonBizConstant
             // 查询参数设定
             // 登陆EmployeeID获得
             NvwaEmployee loginEmployee = this.getEmployeeromSession();
+
             if (loginEmployee == null || loginEmployee.getId() == 0) {
                 setErrMsg(res, "用户ID参数缺失");
                 return res;
@@ -83,6 +88,19 @@ public class AuditController extends BaseController implements CommonBizConstant
             multiSearchBean.addSearchBean(reviewStatusSearch);
 
             PaginationDescribe<RedstarCommunityUpdateLog> result = (PaginationDescribe<RedstarCommunityUpdateLog>) dispatchDriver.getRedstarCommunityUpdateLogManager().searchBeanPage(page, pageSize, multiSearchBean, "updateDate", Boolean.FALSE);
+
+            List lstTemp = result.getCurrentRecords();
+            if ("NEEDACTION".equals(strType)) { // 待审核显示创建时间
+                for (int i = 0; i < lstTemp.size(); i++) {
+                    RedstarCommunityUpdateLog objTemp = (RedstarCommunityUpdateLog) lstTemp.get(i);
+                    objTemp.setAuditShowDate(objTemp.getCreateDate());
+                }
+            } else { // 审核通过与不通过显示更新时间
+                for (int i = 0; i < lstTemp.size(); i++) {
+                    RedstarCommunityUpdateLog objTemp = (RedstarCommunityUpdateLog) lstTemp.get(i);
+                    objTemp.setAuditShowDate(objTemp.getUpdateDate());
+                }
+            }
             res.addKey("result", result);
 
             // 成功与否消息文字设置
@@ -143,6 +161,20 @@ public class AuditController extends BaseController implements CommonBizConstant
             multiSearchBean.addSearchBean(reviewStatusSearch);
 
             PaginationDescribe<RedstarCommunityUpdateLog> resultCommunityUpdateLog = (PaginationDescribe<RedstarCommunityUpdateLog>) dispatchDriver.getRedstarCommunityUpdateLogManager().searchBeanPage(page, pageSize, multiSearchBean, "updateDate", Boolean.FALSE);
+
+            List lstTemp = resultCommunityUpdateLog.getCurrentRecords();
+            if ("NEEDACTION".equals(strType)) { // 待审核显示创建时间
+                for (int i = 0; i < lstTemp.size(); i++) {
+                    RedstarCommunityUpdateLog objTemp = (RedstarCommunityUpdateLog) lstTemp.get(i);
+                    objTemp.setAuditShowDate(objTemp.getCreateDate());
+                }
+            } else { // 审核通过与不通过显示更新时间
+                for (int i = 0; i < lstTemp.size(); i++) {
+                    RedstarCommunityUpdateLog objTemp = (RedstarCommunityUpdateLog) lstTemp.get(i);
+                    objTemp.setAuditShowDate(objTemp.getUpdateDate());
+                }
+            }
+
             res.addKey("result", resultCommunityUpdateLog);
 
             // 成功与否消息文字设置
@@ -152,5 +184,37 @@ public class AuditController extends BaseController implements CommonBizConstant
         }
         return res;
     }
+
+    //查询审核小区详细信息
+    @RequestMapping(value = "/{id}")
+    @ResponseBody
+    public Response getDataItem(@PathVariable("id") Integer id) {
+        PipelineContext pipelineContext = this.buildPipelineContent();
+        Response res = pipelineContext.getResponse();
+
+        try {
+            RedstarCommunityUpdateLog communityUpdateLog = (RedstarCommunityUpdateLog) dispatchDriver.getRedstarCommunityUpdateLogManager().getBean(id);
+            double occupanyRate;
+            double inputRate;
+            if (communityUpdateLog.getAlreadyCheckAmount() != null && communityUpdateLog.getRoomMount() != null && communityUpdateLog.getRoomMount() > 0) {
+                occupanyRate = DoubleUtil.div(communityUpdateLog.getAlreadyCheckAmount(), communityUpdateLog.getRoomMount(), 2);
+                communityUpdateLog.setOccupanyRate(RateUtil.getDoubleValue(occupanyRate * 100));
+            } else {
+                communityUpdateLog.setOccupanyRate(0.0);
+            }
+            if (communityUpdateLog.getAlreadyInputAmount() != null && communityUpdateLog.getRoomMount() != null && communityUpdateLog.getRoomMount() > 0) {
+                inputRate = DoubleUtil.div(communityUpdateLog.getAlreadyInputAmount(), communityUpdateLog.getRoomMount(), 2);
+                communityUpdateLog.setInputRate(RateUtil.getDoubleValue(inputRate * 100));
+            } else {
+                communityUpdateLog.setInputRate(0.0);
+            }
+            res.setCode(HTTP_SUCCESS_CODE);
+            res.addKey("community", communityUpdateLog);
+        } catch (Exception e) {
+            setErrMsg(res, "没有数据");
+        }
+        return res;
+    }
+
 
 }
