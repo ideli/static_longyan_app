34649d0f20aa0d9192b35040deb78d75b2d06be6
zhangxuechao <xuechao.zhang@chianredstar.com>
2016-10-10 16:50:59 +0800

    bug fix

--- longyan-web/src/main/webapp/static_longyan_app/js/api/audit.js
+++ longyan-web/src/main/webapp/static_longyan_app/js/api/audit.js
@@ -68,9 +68,9 @@ define('js/api/audit', ['js/util/http', 'js/api/auth_mock'], function(http, Mock
 			}, false, 'POST', false, $nvwa.header.getHeaders());
 		},
 
-		changeId : function(id, success, error){
+		changeId: function(id, success, error) {
 			var t = this;
-			var url = _basePath + '/'+id;
+			var url = _basePath + '/' + id;
 			var data = data;
 			t._executeRequest(url, data, function(response) {
 				//返回的http请求数据
@@ -78,37 +78,40 @@ define('js/api/audit', ['js/util/http', 'js/api/auth_mock'], function(http, Mock
 			}, false, 'POST', false, $nvwa.header.getHeaders());
 		},
 
-		viewUpdateList : function(id, success, error){
+		viewUpdateList: function(id, success, error) {
 			var t = this;
 			var url = _basePath + '/viewUpdateList';
 			var data = id;
-			t._executeRequest(url, data, function(response){
-					t._executeResponse(response, success, error);
+			t._executeRequest(url, data, function(response) {
+				t._executeResponse(response, success, error);
 			}, false, 'POST', false, $nvwa.header.getHeader());
 		},
 
-		update : function(id, status, success, error){
+		update: function(id, status, success, error) {
 			var t = this;
 			var url = _basePath + '/update';
-			var data = {id:id, status:status}
+			var data = {
+				id: id,
+				status: status
+			}
 			t._executeRequest(url, data, function(response) {
 				//返回的http请求数据
 				t._executeResponse(response, success, error);
 			}, false, 'POST', false, $nvwa.header.getHeaders());
 		},
 
-		auditDetails : function(id, success, error){
+		auditDetails: function(id, success, error) {
 			var t = this;
-			var url = _basePath+'/'+id ;
-			var data = id;
-			t._executeRequest(url,data, function(response) {
+			var url = _basePath + '/' + id;
+			var data = {};
+			t._executeRequest(url, data, function(response) {
 				//返回的http请求数据
 				t._executeResponse(response, success, error);
 			}, false, 'POST', false, $nvwa.header.getHeaders());
 		},
 
 		//我的审核列表
-		myReviewList:function(data, success, error){
+		myReviewList: function(data, success, error) {
 			var t = this;
 			var url = _basePath + '/viewAuditList';
 			var data = data;
